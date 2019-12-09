d280a2c1fa7d8b84b92f7a563b178cffc73e9ce2
clara <clara@mac.com>
2019-12-09 20:39:36 -0300

    section intro-nosotros

--- js/scripts.js
+++ js/scripts.js
@@ -19,8 +19,65 @@ function trazar(){
     );
 }
 
+//Tipear en intro-nosotros
+
+function tipear(){
+    new TypeIt("#escritura", {
+        strings: ["ELEGANTES","MODERNAS","RESPONSIVE"],
+        speed: 100,
+        deleteSpeed: 50,
+        loop: true,
+        breakLines: false,
+    });
+}
+
+//Carrusel2 clientes en intro-nosotros
+
+function carrusel2(){
+    $("#carruselClientes2").owlCarousel({
+        items: 3,
+        margin: 30,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        loop: true,
+        autoplayHoverPause: true,
+    });
+}
+
 //cuando la pagina este lista
 
 window.onload = () => {
     trazar();
+    tipear();
+    carrusel2();    
 };
+
+
+//Controles de video de la section intro-nosotros
+//PLAY
+$("#playVid, #iconoPlayVid").on("click",() => {
+    $("#videoPresentacion").get(0).play();
+    $("#iconoPlayVid").css("opacity",0);
+
+})
+
+
+//PAUSE
+$("#pauseVid").on("click",() => {
+    $("#videoPresentacion").get(0).pause();
+    $("#iconoPlayVid").css("opacity",1);
+})
+//STOP
+$("#stopVid").on("click",() => {
+    $("#videoPresentacion").get(0).load();
+    $("#iconoPlayVid").css("opacity",1);
+})
+
+//SMOOTH SCROLL MENU NAVEGACION
+$(".nav-link").on("click" , function(){
+    const destino =$(this).attr("href");
+    $("html").animate(
+        {scrollTop: $(destino).offset().top},
+        1000,
+    );
+});
